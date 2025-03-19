3f0e9ef72219baff58b29d36aa914c77dd1edd2f
mariavmuniz <soaresmunizm@gmail.com>
2025-03-18 23:16:19 -0300

    Add new routes

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,12 @@
 import express from 'express';
-import RecordController from './record/record.controller';
-import RecordService from './record/record.service';
-import { Router } from 'express';
-import { injector } from './common';
+import routes from './routes';
 
 const app: express.Express = express();
-const router: express.Router = Router();
 const prefix: string = '/api';
 
 app.use(express.json());
 
-app.use(
-    prefix,
-    new RecordController(router, injector.getService(RecordService)).router
-);
+app.use(prefix,routes);
 
 // Rota para healthcheck
 app.use("/_health", (req: express.Request, res: express.Response) => {
