8b6358ba07e1b2ebc5c4af260878dfd0330b5135
acrucha <mcaa@cin.ufpe.br>
2025-03-20 00:20:36 -0300

    feat(common): add request results and errors

---
+++ src/common/errors.ts
@@ -0,0 +1,6 @@
+
+export enum Errors {
+  READ_RECORD = 'Error when adding read record to database.',
+  GET_UNREAD_RECORDS = 'Error when getting unread records from database.',
+  GET_READ_RECORD = 'Error when getting read record from database.',
+}
