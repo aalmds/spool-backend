1c863085a53da5dbdba5f3635c5d4d9fbc301128
acrucha <mcaa@cin.ufpe.br>
2025-03-20 00:21:09 -0300

    fix: use routes in app

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import Env from './env';
 import app from './app';
+import routes from './routes';
+
+app.use(routes);
 
 app.listen(Env.PORT, () => {
     console.log(`Server running at http://localhost:${Env.PORT}`);
 });
+
