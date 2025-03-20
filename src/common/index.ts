acd3fe7c794b60a4dfdead1eb983e0abcbbfc5a6
acrucha <mcaa@cin.ufpe.br>
2025-03-20 00:27:23 -0300

    feat: add read records and alerts routes

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,6 +1,8 @@
 import Injector from "./injector";
 import RecordRepository from "../record/record.repository";
 import RecordService from "../record/record.service";
+import ReadRecordRepository from "../read-record/read-record.repository";
+import ReadRecordService from "../read-record/read-record.service";
 
 export const injector = new Injector();
 
@@ -12,4 +14,14 @@ injector.registerRepository(
 injector.registerService(
     RecordService,
     new RecordService(injector.getRepository(RecordRepository))
+);
+
+injector.registerRepository(
+   ReadRecordRepository, 
+   new ReadRecordRepository()
+);
+
+injector.registerService(
+   ReadRecordService,
+   new ReadRecordService(injector.getRepository(ReadRecordRepository))
 );
