d14633ebea745f297e7b80956e6524c108324854
Maria Luísa dos Santos Silva <mlss@cin.ufpe.br>
2025-03-21 13:57:02 -0300

    Add record controller

--- src/record/record.controller.ts
+++ src/record/record.controller.ts
@@ -1,3 +1,5 @@
+import { Errors } from "../common/errors";
+import { FailureResult, Result, SuccessResult } from "../common/results";
 import RecordService from "./record.service";
 import { Router, Request, Response } from 'express';
 
@@ -10,6 +12,89 @@ class RecordController {
         this.recordService = recordService;
         this.initRoutes();
     }
+
+    async getByChild(req: Request, res: Response) : Promise<void> {
+        try {
+            const childId = Number(req.params.childId);
+            const { filter, limit, page } = req.query;
+            const records = await this.recordService.getRecordsByChild(
+                childId,
+                filter as string,
+                Number(limit),
+                Number(page));
+            new SuccessResult({
+                msg: Result.transformRequestOnMsg(req),
+                data: records
+            }).handle(res);
+        } catch (error) {
+            new FailureResult({
+                msg: Errors.GET_RECORDS
+            }).handle(res);
+        }
+    }
+
+    async getByChildAndTherapist(req: Request, res: Response): Promise<void>  {
+        try {
+            const { childId, therapistId } = req.params;
+            console.log(childId, therapistId);
+            const records = await this.recordService.getRecordsByChildAndTherapist(Number(childId), Number(therapistId));
+            console.log(records);
+            new SuccessResult({
+                msg: Result.transformRequestOnMsg(req),
+                data: records
+            }).handle(res);
+        } catch (error) {
+            new FailureResult({
+                msg: Errors.GET_RECORDS
+            }).handle(res);
+        }
+    }
+
+    async getByChildAndEducationist(req: Request, res: Response) : Promise<void> {
+        try {
+            const { childId, educationistId } = req.params;
+            const records = await this.recordService.getRecordsByChildAndEducationist(Number(childId), Number(educationistId));
+            new SuccessResult({
+                msg: Result.transformRequestOnMsg(req),
+                data: records
+            }).handle(res);
+        } catch (error) {
+            new FailureResult({
+                msg: Errors.GET_RECORDS
+            }).handle(res);
+        }
+    }
+
+    async getByTherapist(req: Request, res: Response) : Promise<void> {
+        try {
+            const therapistId = Number(req.params.therapistId);
+            const status = req.query.status as string;
+            const records = await this.recordService.getRecordsByTherapist(therapistId, status);
+            new SuccessResult({
+                msg: Result.transformRequestOnMsg(req),
+                data: records
+            }).handle(res);
+        } catch (error) {
+            new FailureResult({
+                msg: Errors.GET_RECORDS
+            }).handle(res);
+        }
+    }
+
+    async getByEducationist(req: Request, res: Response) : Promise<void> {
+        try {
+            const educationistId = Number(req.params.educationistId);
+            const status = req.query.status as string;
+            const records = await this.recordService.getRecordsByEducationist(educationistId, status);
+            new SuccessResult({
+                msg: Result.transformRequestOnMsg(req),
+                data: records
+            }).handle(res);
+        } catch (error) {
+            new FailureResult({
+                msg: Errors.GET_RECORDS
+            }).handle(res);
+        }
     }
 
     public initRoutes() {
